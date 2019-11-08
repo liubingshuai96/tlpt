0738240f9c6679fda0e0cb52eb1ca7e8ec73a15e
liubingshuai96 <liu96vip@163.com>
2019-11-08 14:06:01 +0800

    用户管理接口 11-8

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     /**
      * @用户管理页面
      * @time 2017-11-8
-     *
+     * @author liu
      */
     public function user_mange(Request $request)
     {
@@ -18,8 +19,9 @@ class UserController extends Controller
         $name = $request->input('user_name');
         $tel = $request->input('tel');
         $idcard = $request->input('idcard');
+        $email = $request->input('email');
         $created_at = $request->input('created_at');
-        $updated_at = $request->input('uodated_at');
+        $updated_at = $request->input('updated_at');
         if($created_at) {
             $created_at =strtotime($created_at,'00:00:00');
         }
@@ -28,8 +30,8 @@ class UserController extends Controller
         }
         $page = $request->input('page',1);
         $page_size = $request->input('page_size');
-        $field = array('user.id','user.name','user.idcard','user.account');
-        $user_info = User::select_user_info($name,$tel,$idcard,$created_at,$updated_at,$where,$page_size,$page,$field);
+        $field = array('user.id','user.user_name','user.idcard','user.email');
+        $user_info = User::select_user_info($name,$tel,$idcard,$email,$created_at,$updated_at,$where,$page_size,$page,$field);
         self::success("请求成功",$user_info['list'],'',$user_info['total_page'],$user_info['total_data']);
 
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -27,68 +27,45 @@ class User extends Model
 
     /**
      * @Desc 查询所有用户信息
+     * datetime 2019-11-8
      * @Auther liu
      * @Time
      * */
-    static function select_user_info($name='',$phone='',$id_card='',$register_start_time,$register_end_time,$where,$page_size='',$page=1,$field=['*'],$promoter_name=''){
+    static function select_user_info($name='',$tel='',$idcard='',$email = '',$created_at,$updated_at,$where,$page_size='',$page=1,$field=['*'])
+    {
         $page_size = $page_size ? $page_size : env('PAGE_SIZE');
 
         $info = self::where($where);
+
         if($name != ''){
-            $info = $info->where('user_idcard_info.uname','like','%'.$name.'%');
+            $info = $info->where('user.user_name','like','%'.$name.'%');
         }
-        if($phone != ''){
-            $info = $info->where('user.u_account','like','%'.$phone.'%');
+        if($tel != ''){
+            $info = $info->where('user.tel','like','%'.$tel.'%');
         }
-        if($id_card != ''){
-            $info = $info->where('user_idcard_info.idcard_code','like','%'.$id_card.'%');
+        if($email != '') {
+            $info = $info->where('user.email','like','%'.$email.'%');
         }
-        if($promoter_name){
-            $info = $info->where('promoter.nick_name','like','%'.$promoter_name.'%');
-        }
-        if($register_start_time && $register_end_time){
-            $info = $info->whereBetween('user.ctime',[$register_start_time,$register_end_time]);
-        }else if($register_start_time){
-            $info = $info->where('user.ctime','>',$register_start_time);
-        }else if($register_end_time){
-            $info = $info->where('user.ctime','<',$register_end_time);
+        if($idcard != ''){
+            $info = $info->where('user.idcard','like','%'.$idcard.'%');
         }
 
 
+        if($created_at && $updated_at){
+            $info = $info->whereBetween('user.ctime',[$created_at,$updated_at]);
+        }else if($created_at){
+            $info = $info->where('user.created_at','>',$created_at);
+        }else if($updated_at){
+            $info = $info->where('user.created_at','<',$updated_at);
+        }
 
-        $info = $info->orderBy('user.ctime','desc')
-            ->leftjoin('user_idcard_info','user_idcard_info.user_id','=','user.id')
-            ->leftjoin('promoter','promoter.id','=','user.promoter_id')
-            ->with('user_identify')
+        $info = $info->orderBy('user.created_at','desc')
             ->paginate($page_size,$field,null,$page);
 
-
         $record_list = object_to_array($info);
-        $array = array("mailst"=>0,"operate"=>0,"contacts"=>0,"face"=>0,"alipay"=>0);
-
-        foreach($record_list['data'] as $key=>$value){
-            $record_list['data'][$key] = array_merge($record_list['data'][$key],$array);
-
-            foreach($value['user_identify'] as $k=>$v){
-                if($v['identify_name'] == 'mailst_identify'){
-                    $record_list['data'][$key]['mailst'] = $v['state'];
-                }elseif($v['identify_name'] == 'operator_identify'){
-                    $record_list['data'][$key]['operate'] = $v['state'];
-                }elseif($v['identify_name'] == 'contacts_identify'){
-                    $record_list['data'][$key]['contacts'] = $v['state'];
-                }elseif($v['identify_name'] == 'face_identify'){
-                    $record_list['data'][$key]['face'] = $v['state'];
-                }elseif($v['identify_name'] == 'alipay_identify'){
-                    $record_list['data'][$key]['alipay'] = $v['state'];
-                }
-            }
-        }
-
-
         $result['total_page'] = $record_list['last_page'];
         $result['total_data'] = $record_list['total'];
         $result['list'] = $record_list['data'];
-
         return $result;
     }
 }
